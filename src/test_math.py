0bc2d9932f916126fb746c79e92d5459c123781a
Masahiro Watanabe <m@nsocean.org>
2016-12-12 17:43:52 +0900

    Changed args

--- src/test_math.py
+++ src/test_math.py
@@ -4,7 +4,7 @@ def test_add():
     assert Math.add(10, 4) == 14
 
 def test_subtract():
-    assert Math.subtract(10, 4) == 6
+    assert Math.subtract(10, 10) == 0
 
 def test_multiply():
     assert Math.multiply(10, 3) == 30
